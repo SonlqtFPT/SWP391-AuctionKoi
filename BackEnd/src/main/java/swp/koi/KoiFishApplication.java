683a2f5708eef0b6c453876e5958a7e3ca88726f
monlai-dev <longmtse184140@fpt.edu.vn>
2024-10-16 13:32:15 +0700

    generate qr and invoice to pdf ?

--- BackEnd/src/main/java/swp/koi/KoiFishApplication.java
+++ BackEnd/src/main/java/swp/koi/KoiFishApplication.java
@@ -2,8 +2,13 @@ package swp.koi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.converter.BufferedImageHttpMessageConverter;
+import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import java.awt.image.BufferedImage;
 
 @SpringBootApplication
 @EnableScheduling
@@ -14,4 +19,8 @@ public class KoiFishApplication {
 		SpringApplication.run(KoiFishApplication.class, args);
 	}
 
+	@Bean
+	public HttpMessageConverter<BufferedImage> createImageHttpMessageConverter() {
+		return new BufferedImageHttpMessageConverter();
+	}
 }
