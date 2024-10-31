c1793ceb82054567ee0157bdee41df914987f70d
monlai-dev <longmtse184140@fpt.edu.vn>
2024-10-31 11:10:56 +0700

    fix transaction response

--- BackEnd/src/main/java/swp/koi/convert/TransactionEntityToDtoConverter.java
+++ BackEnd/src/main/java/swp/koi/convert/TransactionEntityToDtoConverter.java
@@ -7,6 +7,7 @@ import swp.koi.dto.response.TransactionResponseDto;
 import swp.koi.model.Transaction;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Component
@@ -19,8 +20,18 @@ public class TransactionEntityToDtoConverter {
     public List<TransactionResponseDto> convertTransactionList(List<Transaction> transactions){
         List<TransactionResponseDto> response = transactions.stream().map(transaction -> {
             TransactionResponseDto dto = modelMapper.map(transaction, TransactionResponseDto.class);
-            dto.setLotId(transaction.getLot().getLotId());
-            dto.setMember(memberEntityToDtoConverter.convertMember(transaction.getMember()));
+
+            if (transaction.getLot() != null || transaction.getMember() != null) {
+                Integer lotId = transaction.getLot().getLotId();
+                dto.setLotId(lotId);
+
+                dto.setMember(memberEntityToDtoConverter.convertMember(transaction.getMember()));
+
+            } else {
+                dto.setLotId(null);
+                dto.setMember(null);
+            }
+
 
             return dto;
         }).collect(Collectors.toList());
